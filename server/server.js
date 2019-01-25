9dc49fdb4be51e7d0650e0fd51981038d266f90a
Nikola Milivojevic <nikola.milivojevic@codecentric.de>
2019-01-25 10:07:15 +0100

    no need for access control headers

--- server/server.js
+++ server/server.js
@@ -6,13 +6,6 @@ const routes = require('./routes')
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
 
-server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 server.use(routes);
 
 module.exports = server
